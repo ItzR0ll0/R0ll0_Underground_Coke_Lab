efb437db80d2bb22f3db63025f6a84c9c18adadb
ItzR0ll0 <dude30804@gmail.com>
2026-07-07 07:59:37 +0200

    Add files via upload

---
+++ R0ll0_Underground_Coke_lab/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+version '1.0'
+author 'R0ll0'
+description 'Underground Coke Lab MLO'
+
+files {
+    'r0ll0_coke_lab_manifest.ymf',
+
+    'stream/[ybn]/*.ybn',
+    'stream/[ydr]/*.ydr',
+    'stream/[ymap]/*.ymap',
+    'stream/[ytyp]/*.ytyp',
+
+    'stream/[vanilla]/*.ybn',
+    'stream/[vanilla]/*.ydr',
+    'stream/[vanilla]/*.ymap'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'r0ll0_coke_lab_manifest.ymf'
